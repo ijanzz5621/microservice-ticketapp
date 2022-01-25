f7fa5838c27d2d0222e6fde66cb559cdb68c7d2c
Sharizan Redzuan <ijanzz5621@gmail.com>
2022-01-25 16:34:52 +0800

    add routing for users

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+// import the router
+import { currentUserRouter } from './routes/current-user';
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send('Hi User!');
-});
+// use the router
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log("Auth Service - Listening at port 3000!!!");
